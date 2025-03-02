500479d9347681a754afc5aee453f511bce1222e
subhajyoti-prusty <subhajyoti.2004@gmail.com>
2025-03-02 11:52:42 +0530

    Optimization done

--- Arrays/Max_Min_Element_in_Array.java
+++ Arrays/Max_Min_Element_in_Array.java
@@ -32,3 +32,24 @@ public class Max_Min_Element_in_Array {
         System.out.println("Maximum element is: " + Max(A, N));
     }
 }
+
+// public static void findMinMax(int[] A) {
+//     int min = A[0], max = A[0];
+
+//     // Single pass to find both min and max
+//     for (int i = 1; i < A.length; i++) {
+//         if (A[i] < min) {
+//             min = A[i];
+//         } else if (A[i] > max) {
+//             max = A[i];
+//         }
+//     }
+
+//     System.out.println("Minimum element is: " + min);
+//     System.out.println("Maximum element is: " + max);
+// }
+
+// public static void main(String[] args) {
+//     int[] A = { 4, 9, 6, 5, 2, 3 };
+//     findMinMax(A);
+// }
