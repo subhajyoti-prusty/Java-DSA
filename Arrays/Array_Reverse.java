46dc6ad3e468f5da2cc6a16dfccb6aaa42d45304
subhajyoti-prusty <subhajyoti.2004@gmail.com>
2025-03-15 14:14:09 +0530

    Reversing an Array

--- Arrays/Array_Reverse.java
+++ Arrays/Array_Reverse.java
@@ -2,28 +2,32 @@
     Input: arr[] = {1, 4, 3, 2, 6, 5}  
     Output: {5, 6, 2, 3, 4, 1}
 
-    Explanation: The first element 1 moves to last position, the second element 4 moves to second-last and so on.
+    Explanation: The Firstirst element 1 moves to last position, the second element 4 moves to second-last and so on.
  */
 
 public class Array_Reverse {
 
-    public static void reverseArray(int arr[], int n){
+    public static void reverseArray(int arr[]){
+        int First = 0 , Last = arr.length - 1;
 
-        int arr1[] =  new int[n];
+        while (First < Last){
+            int temp = arr[First];
+            arr[First] = arr[Last];
+            arr[Last] = temp;
 
-        for (int i = 0; i < arr.length; i++) {
-            arr1[i] = arr[n-i-1];
+            First++;
+            Last--;
         }
 
-        for (int i = 0; i < arr1.length; i++) {
-            System.out.print(arr1[i] + " ");
-        }
     }
+
     public static void main(String[] args) {
         int[] arr = { 1, 4, 3, 2, 6, 5 };
 
-        int n = 6;
+        reverseArray(arr);
 
-        reverseArray(arr,n);
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
