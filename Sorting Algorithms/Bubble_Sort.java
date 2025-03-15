079c0f53df825711907315a2a69a0411db4b0358
subhajyoti-prusty <subhajyoti.2004@gmail.com>
2025-03-15 14:10:34 +0530

    Bubble Sorting

---
+++ Sorting Algorithms/Bubble_Sort.java
@@ -0,0 +1,25 @@
+public class Bubble_Sort {
+    public static void sort(int arr[]) {
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
+
+        sort(arr);
+    }
+
+}
