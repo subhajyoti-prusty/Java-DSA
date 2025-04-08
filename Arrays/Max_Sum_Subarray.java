9d998338d01aeae8ace54a90f6f6b45e2155c15f
subhajyoti-prusty <subhajyoti.2004@gmail.com>
2025-04-09 02:54:29 +0530

    Using Kadane's Algorithm

--- Arrays/Max_Sum_Subarray.java
+++ Arrays/Max_Sum_Subarray.java
@@ -19,8 +19,29 @@ public class Max_Sum_Subarray {
         System.out.println("Total Sum of All Subarrays: " + totalSum);
     }
 
+    public static int kadanesAlgorithm(int arr[]) {
+        if(arr.length == 1) return arr[0];
+        int currentsum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            currentsum = currentsum + arr[i];
+
+            maxSum = Math.max(currentsum, maxSum);
+
+            if (currentsum < 0) {
+                currentsum = 0;
+            }
+        }
+        return maxSum;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 2, 4, 6, 8, 10 };
         maxSumSubarray(arr);
+
+        int arr1[] = { -2, -4, -6, -8, -10, -12, -14 };
+        int maxValue  = kadanesAlgorithm(arr1);
+        System.out.println(maxValue);
     }
 }
